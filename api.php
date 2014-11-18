dbdbe49c1ac9cc64a98e56e5e709f1ee8a62e19d
Marko Heijnen <info@markoheijnen.com>
2014-11-18 16:00:44 +0100

    First version

---
+++ api.php
@@ -0,0 +1,380 @@
+<?php
+
+class WordPress_Stats_Api {
+
+	public static function db_table() {
+		global $wpdb;
+
+		return $wpdb->prefix . 'wordpress_stats';
+	}
+
+	public static function wp_version() {
+		return WordPress_Stats::latest_version();
+	}
+
+
+	public static function downloads_per_day() {
+		global $wpdb;
+
+		if ( false === ( $data = get_transient( 'wordpress_downloads_day' ) ) ) {
+			$query = "SELECT MAX(count) as count, Date(date_gmt) as date, date_format(date_gmt,'%m/%d/%Y') as date_display FROM " . self::db_table() . " WHERE type='downloads' AND version = '" . self::wp_version() . "' GROUP BY YEAR(date_gmt), MONTH(date_gmt), DATE(date_gmt) ORDER BY date_gmt";
+			$data = $wpdb->get_results( $query );
+
+			$data = array_filter( $data, array( __CLASS__, 'make_value_number' ) );
+
+			set_transient( 'wordpress_downloads_day', $data, 60 );
+		}
+
+		return $data;
+	}
+
+	public static function wordpress_downloads() {
+		global $wpdb;
+
+		if ( false === ( $count = get_transient( 'wordpress_downloads2' ) ) ) {
+			$query = "SELECT count FROM " . self::db_table() . " WHERE type='downloads' AND version = '" . self::wp_version() . "' ORDER BY date_gmt DESC LIMIT 1";
+			$count = $wpdb->get_row( $query );
+			$count = number_format( $count->count );
+
+			set_transient( 'wordpress_downloads', $count, 60 );
+		}
+
+		return $count;
+	}
+
+	public static function downloads_last7days() {
+		global $wpdb, $wp_locale;
+
+		if ( false === ( $count = get_transient( 'downloads_last7days' ) ) ) {
+			$query = "SELECT ( MAX(count) - MIN(count) ) as downloads, WEEKDAY( date_gmt ) as weekday FROM " . self::db_table() . " WHERE type='downloads' AND version = '" . self::wp_version() . "' GROUP BY YEAR(date_gmt), MONTH(date_gmt), DATE(date_gmt) ORDER BY date_gmt DESC LIMIT 7";
+			$data = $wpdb->get_results( $query );
+
+			$count = array();
+
+			foreach( $data as $row ) {
+				$weekday = ( $row->weekday == 6 ) ? 0 : $row->weekday + 1;
+
+				$count[] = array( 'label' => $weekday, 'value' => absint( $row->downloads ) );
+			}
+
+			set_transient( 'downloads_last7days', $count, 60 );
+		}
+
+
+		for( $i = 0; $i < count( $count ); ++$i ) {
+			$count[ $i ]['label'] = $wp_locale->get_weekday( $count[ $i ]['label'] );
+		}
+
+		return $count;
+	}
+
+	public static function counts_per_hour() {
+		$data  = self::get_counts_data( 'hours' );
+		$hours = array();
+
+		foreach( $data as $hour => $value ) {
+			$hours[] = array( 'label' => $hour, 'value' => $value );
+		}
+
+		return $hours;
+	}
+
+	public static function counts_per_day() {
+		global $wp_locale;
+
+		$data = self::get_counts_data( 'days' );
+		$days = array();
+
+		foreach( $data as $day => $value ) {
+			$days[] = array( 'label' => $wp_locale->get_weekday( $day ), 'value' => $value );
+		}
+
+		return $days;
+	}
+
+	private static function get_counts_data( $type ) {
+		global $wpdb;
+
+		if( 'hours' != $type && 'days' != $type ) {
+			return array();
+		}
+
+		if ( false === ( $data = get_transient( 'wordpress_counts_' . $type ) ) ) {
+			$query = "SELECT ( MAX(count) - MIN(count) ) as downloads, WEEKDAY( date_gmt ) as weekday, HOUR( date_gmt ) as hour FROM " . self::db_table() . " WHERE type='downloads' AND version = '" . self::wp_version() . "' GROUP BY YEAR(date_gmt), MONTH(date_gmt), DATE(date_gmt), HOUR(date_gmt)";
+			$rows  = $wpdb->get_results( $query );
+
+			$days = array( 0, 0, 0, 0, 0, 0, 0 );
+			$hours = array( 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 );
+
+			foreach( $rows as $row ) {
+				$days[ $row->weekday ] = $days[ $row->weekday ] + $row->downloads;
+				$hours[ $row->hour ] = $hours[ $row->hour ] + $row->downloads;
+			}
+
+			set_transient( 'wordpress_counts_days', $days, 60 );
+			set_transient( 'wordpress_counts_hours', $hours, 60 );
+
+			$data = $$type;
+		}
+
+		return $data;
+	}
+
+
+	public static function get_major_releases() {
+		return array(
+			'3.3',
+			'3.4',
+			'3.5',
+			'3.6',
+			'3.7',
+			'3.8',
+			'3.9'
+		);
+	}
+
+	public static function get_minor_releases( $major = null ) {
+		if( $major == null ) {
+			$major = self::wp_version();
+		}
+
+		$releases = array(
+			'4.0' => array(
+				'4.0' => array(
+					'title' => 'WordPress 4.0 “Benny”',
+					'link'  => 'https://wordpress.org/news/2014/09/benny/'
+				)
+			),
+			'3.9' => array(
+				'3.9.2' => array(
+					'title' => 'WordPress 3.9.2 Security Release',
+					'link'  => 'https://wordpress.org/news/2014/08/wordpress-3-9-2/'
+				),
+				'3.9.1' => array(
+					'title' => 'WordPress 3.9.1 Maintenance Release',
+					'link'  => 'https://wordpress.org/news/2014/05/wordpress-3-9-1/'
+				),
+				'3.9' => array(
+					'title' => 'WordPress 3.9 “Smith”',
+					'link'  => 'https://wordpress.org/news/2014/04/smith/'
+				)
+			),
+			'3.8' => array(
+				'3.8.3' => array(
+					'title' => 'WordPress 3.8.3 Maintenance Release',
+					'link'  => 'https://wordpress.org/news/2014/04/wordpress-3-8-3/'
+				),
+				'3.8.2' => array(
+					'title' => 'WordPress 3.8.2 Security Release',
+					'link'  => 'https://wordpress.org/news/2014/04/wordpress-3-8-2/'
+				),
+				'3.8.1' => array(
+					'title' => 'WordPress 3.8.1 Maintenance Release',
+					'link'  => 'https://wordpress.org/news/2014/01/wordpress-3-8-1/'
+				),
+				'3.8' => array(
+					'title' => 'WordPress 3.8 “Parker”',
+					'link'  => 'https://wordpress.org/news/2013/12/parker/'
+				)
+			),
+			'3.7' => array(
+				'3.7.1' => array(
+					'title' => 'WordPress 3.7.3 Security Release',
+					'link'  => 'https://wordpress.org/news/2014/04/wordpress-3-8-2/'
+				),
+				'3.7.1' => array(
+					'title' => 'WordPress 3.7.1 Maintenance Release',
+					'link'  => 'https://wordpress.org/news/2013/10/wordpress-3-7-1/'
+				),
+				'3.7' => array(
+					'title' => 'WordPress 3.7 “Basie”',
+					'link'  => 'https://wordpress.org/news/2013/10/basie/'
+				)
+			),
+			'3.6' => array(
+				'3.6.1' => array(
+					'title' => 'WordPress 3.6.1 Maintenance and Security Release',
+					'link'  => 'https://wordpress.org/news/2013/09/wordpress-3-6-1/'
+				),
+				'3.6' => array(
+					'title' => 'WordPress 3.6 “Oscar”',
+					'link'  => 'https://wordpress.org/news/2013/08/oscar/'
+				)
+			),
+			'3.5' => array(
+				'3.5.2' => array(
+					'title' => 'WordPress 3.5.2 Maintenance and Security Release',
+					'link'  => 'https://wordpress.org/news/2013/06/wordpress-3-5-2/'
+				),
+				'3.5.1' => array(
+					'title' => 'WordPress 3.5.1 Maintenance and Security Release',
+					'link'  => 'https://wordpress.org/news/2013/01/wordpress-3-5-1/'
+				),
+				'3.5' => array(
+					'title' => 'WordPress 3.5 “Elvin”',
+					'link'  => 'https://wordpress.org/news/2012/12/elvin/'
+				)
+			),
+			'3.4' => array(
+				'3.4.2' => array(
+					'title' => 'WordPress 3.4.2 Maintenance and Security Release',
+					'link'  => 'https://wordpress.org/news/2012/09/wordpress-3-4-2/'
+				),
+				'3.4.1' => array(
+					'title' => 'WordPress 3.4.1 Maintenance and Security Release',
+					'link'  => 'https://wordpress.org/news/2012/06/wordpress-3-4-1/'
+				),
+				'3.4' => array(
+					'title' => 'WordPress 3.4 “Green”',
+					'link'  => 'https://wordpress.org/news/2012/06/green/'
+				)
+			),
+			'3.3' => array(
+				'3.3.1' => array(
+					'title' => 'WordPress 3.3.1 Security and Maintenance Release',
+					'link'  => 'https://wordpress.org/news/2012/01/wordpress-3-3-1/'
+				),
+				'3.3' => array(
+					'title' => 'WordPress 3.3 “Sonny”',
+					'link'  => 'https://wordpress.org/news/2011/12/sonny/'
+				)
+			),
+		);
+
+		if( isset( $releases[ $major ] ) ) {
+			return $releases[ $major ];
+		}
+
+		return array();
+	}
+
+
+	public static function wordpress_version() {
+		if ( false === ( $data = get_transient( 'wordpress_versions' ) ) ) {
+			global $wpdb;
+
+			$query = "SELECT s1.count as value, s1.version as label FROM " . self::db_table() . " as s1 LEFT JOIN " . self::db_table() . " s2 ON (s1.type = s2.type AND s1.date_gmt < s2.date_gmt) WHERE s1.type='wordpress' AND s2.type IS NULL AND s1.date_gmt > DATE_SUB(CURDATE(), INTERVAL 25 HOUR)";
+			$data = $wpdb->get_results( $query );
+
+			$data = array_values( array_filter( $data, array( __CLASS__, 'make_value_number' ) ) );
+
+			set_transient( 'wordpress_versions', $data, HOUR_IN_SECONDS );
+		}
+
+		return $data;
+	}
+
+	public static function php_version() {
+		if ( false === ( $data = get_transient( 'php_versions' ) ) ) {
+			global $wpdb;
+
+			$query = "SELECT s1.count as value, s1.version as label FROM " . self::db_table() . " as s1 LEFT JOIN " . self::db_table() . " s2 ON (s1.type = s2.type AND s1.date_gmt < s2.date_gmt) WHERE s1.type='php' AND s2.type IS NULL AND s1.date_gmt > DATE_SUB(CURDATE(), INTERVAL 25 HOUR)";
+			$data = $wpdb->get_results( $query );
+
+			$data = array_values( array_filter( $data, array( __CLASS__, 'make_value_number' ) ) );
+
+			set_transient( 'php_versions', $data, HOUR_IN_SECONDS );
+		}
+
+		return $data;
+	}
+
+	public static function mysql_version() {
+		if ( false === ( $data = get_transient( 'mysql_versions' ) ) ) {
+			global $wpdb;
+
+			$query = "SELECT s1.count as value, s1.version as label FROM " . self::db_table() . " as s1 LEFT JOIN " . self::db_table() . " s2 ON (s1.type = s2.type AND s1.date_gmt < s2.date_gmt) WHERE s1.type='mysql' AND s2.type IS NULL AND s1.date_gmt > DATE_SUB(CURDATE(), INTERVAL 25 HOUR)";
+			$data = $wpdb->get_results( $query );
+
+			$data = array_values( array_filter( $data, array( __CLASS__, 'make_value_number' ) ) );
+
+			set_transient( 'mysql_versions', $data, HOUR_IN_SECONDS );
+		}
+
+		return $data;
+	}
+
+
+	public static function wordpress_version_by_day() {
+		global $wpdb;
+
+		//$query = "SELECT version, GROUP_CONCAT(count) as mycount FROM wpc_wordpress_stats WHERE type='php' GROUP BY version";
+		$query = "SELECT date_format(date_gmt,'%Y-%m-%d') as date, GROUP_CONCAT(count) as counts, GROUP_CONCAT(version) as versions FROM wpc_wordpress_stats WHERE type='wordpress' AND version NOT IN ('2.7', '2.8', '2.9') GROUP BY date_gmt";
+		$data = $wpdb->get_results( $query );
+
+		foreach ( $data as $key => $row ) {
+			$counts   = explode(',', $row->counts );
+			$versions = explode(',', $row->versions );
+
+			foreach ( $versions as $key2 => $version ) {
+				$data[ $key ]->$version = (float) $counts[ $key2 ];
+			}
+
+			unset( $data[ $key ]->counts );
+			unset( $data[ $key ]->versions );
+		}
+
+		return $data;
+	}
+
+	public static function php_version_by_day() {
+		global $wpdb;
+
+		$query = "SELECT date_format(date_gmt,'%Y-%m-%d') as date, GROUP_CONCAT(count) as counts, GROUP_CONCAT(version) as versions FROM wpc_wordpress_stats WHERE type='php' AND version NOT IN ('4.3', '4.4', '5.0', '5.6', '5.7') GROUP BY date_gmt";
+		$data = $wpdb->get_results( $query );
+
+		foreach ( $data as $key => $row ) {
+			$counts   = explode(',', $row->counts );
+			$versions = explode(',', $row->versions );
+
+			foreach ( $versions as $key2 => $version ) {
+				$data[ $key ]->$version = (float) $counts[ $key2 ];
+			}
+
+			unset( $data[ $key ]->counts );
+			unset( $data[ $key ]->versions );
+		}
+
+		return $data;
+	}
+
+	public static function mysql_version_by_day() {
+		global $wpdb;
+
+		$query = "SELECT date_format(date_gmt,'%Y-%m-%d') as date, GROUP_CONCAT(count) as counts, GROUP_CONCAT(version) as versions FROM wpc_wordpress_stats WHERE type='mysql' AND version NOT IN ('3.23', '4.0', '4.1', '5.', '5.13', '5.2', '5.3', '5.4', '5.7') GROUP BY date_gmt";
+		$data = $wpdb->get_results( $query );
+
+		foreach ( $data as $key => $row ) {
+			$counts   = explode(',', $row->counts );
+			$versions = explode(',', $row->versions );
+
+			foreach ( $versions as $key2 => $version ) {
+				$data[ $key ]->$version = (float) $counts[ $key2 ];
+			}
+
+			unset( $data[ $key ]->counts );
+			unset( $data[ $key ]->versions );
+		}
+
+		return $data;
+	}
+
+
+	public static function make_value_number( $item ) {
+		if ( isset( $item->value ) ) {
+			$item->value = (float) $item->value;
+
+			if ( $item->value ) {
+				return $item;
+			}
+		}
+		else if ( isset( $item->count ) ) {
+			$item->count = (float) $item->count;
+
+			return $item;
+		}
+
+		return false;
+	}
+
+}
---
+++ wordpress-stats.php
@@ -0,0 +1,209 @@
+<?php
+/*
+Plugin Name: WordPress stats
+Plugin URI:  http://wpcentral.io
+Description: Retrieves all stats provided by WordPress.org
+Author:      Marko Heijnen
+Text Domain: wordpress-stats
+Version:     1.0
+Author URI:  http://markoheijnen.com
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
+
+include 'api.php';
+
+class WordPress_Stats {
+	private $version = '1.0';
+	private $api_url = 'http://api.wordpress.org/stats/';
+
+	private $db_table;
+	private $request_time;
+
+	public function __construct() {
+		$this->db_table = WordPress_Stats_Api::db_table();
+
+		register_activation_hook( __FILE__, array( $this, 'install' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+
+		add_filter( 'cron_schedules', array( $this, 'cron_schedules' ) );
+		add_action( 'cron_wordpress_stats', array( $this, 'cronjob_fast' ) );
+		add_action( 'cron_wordpress_stats_daily', array( $this, 'cronjob_daily' ) );
+	}
+
+	public function install() {
+		global $wpdb;
+
+		if( $wpdb->get_var( "SHOW TABLES LIKE '" . $this->db_table . "'" ) != $this->db_table ) {
+			$sql = "CREATE TABLE IF NOT EXISTS " . $this->db_table . " (
+				type varchar(32) NOT NULL,
+				version varchar(8) NOT NULL,
+				count DECIMAL(10,1) NOT NULL,
+				date_gmt datetime NOT NULL
+			);";
+
+			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+			dbDelta( $sql );
+		}
+
+		if ( ! wp_next_scheduled( 'cron_wordpress_stats' ) ) {
+			date_default_timezone_set("UTC");
+			$time = time();
+			$time = ceil( $time / ( 1 * 60 ) ) * ( 1 * 60 );
+
+			wp_schedule_event( $time, 'minutly', 'cron_wordpress_stats' );
+
+			wp_schedule_event( strtotime( 'today 7am' ), 'daily', 'cron_wordpress_stats_daily' );
+		}
+	}
+
+	public function deactivate() {
+		wp_clear_scheduled_hook( 'cron_wordpress_stats' );
+		wp_clear_scheduled_hook( 'cron_wordpress_stats_daily' );
+	}
+
+
+
+
+	public function cron_schedules( $schedules ) {
+		$schedules['minutly'] = array( 'interval' => MINUTE_IN_SECONDS, 'display' => __( 'Every minute', 'wordpress-stats' ) );
+
+		return $schedules;
+	}
+
+	// Type = Downloads, MySQL, PHP, WordPress
+	public function cronjob_fast() {
+		$this->add_stat( 'downloads', $this->latest_version(), $this->get_current_wordpress_downloads() );
+	}
+
+	public function cronjob_daily() {
+		$wordpress = $this->get_current_wordpress_usage();
+		foreach( $wordpress as $version => $percentage ) {
+			$this->add_stat( 'wordpress', $version, $percentage );
+		}
+
+		$php = $this->get_current_php_usage();
+		foreach( $php as $version => $percentage ) {
+			$this->add_stat( 'php', $version, $percentage );
+		}
+
+		$mysql = $this->get_current_mysql_usage();
+		foreach( $mysql as $version => $percentage ) {
+			$this->add_stat( 'mysql', $version, $percentage );
+		}
+
+		// Delete the caches
+		delete_transient('wordpress_versions');
+		delete_transient('php_versions');
+		delete_transient('mysql_versions');
+	}
+
+
+
+	public static function latest_version() {
+		if ( false === ( $version = get_transient( 'wordpress_version' ) ) ) {
+			$request = wp_remote_get( 'http://api.wordpress.org/core/version-check/1.7/' );
+			$data    = json_decode( wp_remote_retrieve_body( $request ) );
+
+			if( $data ) {
+				$version = explode( '.', $data->offers[0]->current );
+				$version = $version[0] . '.' . $version[1];
+
+				set_transient( 'wordpress_version', $version, 3600 * 6 );
+			}
+		}
+
+		return $version;
+	}
+
+	public static function get_current_wordpress_downloads() {
+		$response = wp_remote_get( 'http://wordpress.org/download/counter/?ajaxupdate=1&time=' . time() );
+
+		if( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) != 200 ) {
+			self::report_error( $response );
+
+			return false;
+		}
+		else {
+			$count = wp_remote_retrieve_body( $response );
+
+			$count = str_replace( '.', '', $count );
+			$count = str_replace( ',', '', $count );
+			
+			return $count;
+		}
+	}
+
+	public function get_current_wordpress_usage() {
+		$response = wp_remote_get( $this->api_url . 'wordpress/1.0/' );
+
+		if( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) != 200 ) {
+			$this->report_error( $response );
+
+			return false;
+		}
+		else {
+			$data = json_decode( wp_remote_retrieve_body( $response ) );
+
+			return $data;
+		}
+	}
+
+	public function get_current_php_usage() {
+		$response = wp_remote_get( $this->api_url . 'php/1.0/' );
+
+		if( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) != 200 ) {
+			$this->report_error( $response );
+
+			return false;
+		}
+		else {
+			$data = json_decode( wp_remote_retrieve_body( $response ) );
+
+			return $data;
+		}
+	}
+
+	public function get_current_mysql_usage() {
+		$response = wp_remote_get( $this->api_url . 'mysql/1.0/' );
+
+		if( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) != 200 ) {
+			$this->report_error( $response );
+
+			return false;
+		}
+		else {
+			$data = json_decode( wp_remote_retrieve_body( $response ) );
+
+			return $data;
+		}
+	}
+
+
+
+	public function add_stat( $type, $version, $count ) {
+		global $wpdb;
+
+		if( ! $this->request_time ) {
+			$this->request_time = get_gmt_from_date( current_time( 'mysql' ) );
+		}
+
+		$data             = array();
+		$data['type']     = $type;
+		$data['version']  = $version;
+		$data['count']    = $count;
+		$data['date_gmt'] = $this->request_time;
+		
+		$wpdb->insert( $this->db_table, $data, array( '%s', '%s', '%f', '%s' ) );
+	}
+
+
+	private static function report_error( $response ) {
+		
+	}
+
+}
+
+new WordPress_Stats;
